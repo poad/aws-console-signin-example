9b98f300b9fc06d99d58cea4c14fe121c79a98c3
Kenji Saito <ken-yo@mbr.nifty.com>
2023-09-02 19:00:55 +0900

    Add the cognito Sign in example

---
+++ cognito-admin/app/next.config.js
@@ -0,0 +1,15 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer');
+
+/** @type {import('next').NextConfig} */
+const config = {
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: false,
+  experimental: {
+    esmExternals: true,
+  }
+};
+
+module.exports = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(config);
