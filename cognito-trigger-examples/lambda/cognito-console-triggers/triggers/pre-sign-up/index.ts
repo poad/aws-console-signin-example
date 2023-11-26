885ec6adea0de8e940f96e4cc3c70b64cdfe4d12
Kenji Saito <ken-yo@mbr.nifty.com>
2023-11-26 18:20:22 +0900

    Update the lambda runtime to 20

--- cognito-trigger-examples/lambda/cognito-console-triggers/triggers/pre-sign-up/index.ts
+++ cognito-trigger-examples/lambda/cognito-console-triggers/triggers/pre-sign-up/index.ts
@@ -1,13 +1,20 @@
 /* eslint-disable import/prefer-default-export */
-import { CognitoIdentityProviderClient, ListUsersCommand } from '@aws-sdk/client-cognito-identity-provider';
 import {
-  Callback, Context, PreSignUpTriggerEvent, PreSignUpTriggerHandler,
+  CognitoIdentityProviderClient,
+  ListUsersCommand,
+  AdminCreateUserCommand,
+} from '@aws-sdk/client-cognito-identity-provider';
+import {
+  Callback,
+  Context,
+  PreSignUpTriggerEvent,
+  PreSignUpTriggerHandler,
 } from 'aws-lambda';
 
 export const handler: PreSignUpTriggerHandler = async (
   event: PreSignUpTriggerEvent,
   _: Context,
-  callback: Callback<any>,
+  callback: Callback<any>
 ): Promise<any> => {
   // console.log(JSON.stringify(event));
 
@@ -19,20 +26,54 @@ export const handler: PreSignUpTriggerHandler = async (
 
     const identityProvider = new CognitoIdentityProviderClient({});
 
-    const user = (await identityProvider.send(new ListUsersCommand({
-      UserPoolId: userPoolId,
-      Filter: `email = "${email}"`,
-    }))).Users?.find((u) => u.UserStatus !== 'EXTERNAL_PROVIDER');
+    const user = (
+      await identityProvider.send(
+        new ListUsersCommand({
+          UserPoolId: userPoolId,
+          Filter: `email = "${email}"`,
+        })
+      )
+    ).Users?.find(
+      (u) => (u.UserStatus as string | undefined) !== 'EXTERNAL_PROVIDER'
+    );
 
-    if (user === undefined) {
+    // create a new user
+    const targetUser =
+      user ??
+      (
+        await identityProvider.send(
+          new AdminCreateUserCommand({
+            UserPoolId: userPoolId,
+            Username: request.userAttributes.email,
+            UserAttributes: Object.entries(request.userAttributes)
+              .filter(
+                (attr) =>
+                  attr[0] !== 'cognito:email_alias' &&
+                  attr[0] !== 'cognito:phone_number_alias'
+              )
+              .map((attr) => ({ Name: attr[0], Value: attr[1] })),
+          })
+        )
+      ).User;
+    if (!targetUser) {
       return callback('No such link target', event);
     }
 
     const provider = event.userName.split('_')[0];
-    const identities: { [name: string]: string }[] = (user.Attributes || [])
-      .filter((attribute) => attribute.Name === 'identities' && attribute.Value !== undefined)
-      .flatMap((attribute) => JSON.parse(attribute.Value!) as { [name: string]: string }[]);
-    if (identities.find((identity) => identity.providerName !== undefined && identity.providerName === provider) === undefined) {
+    const identities: { [name: string]: string }[] = (
+      targetUser.Attributes ?? []
+    )
+      .filter((attribute) => attribute.Name === 'identities' && attribute.Value)
+      .flatMap(
+        (attribute) =>
+          JSON.parse(attribute.Value!) as { [name: string]: string }[]
+      );
+    if (
+      !identities.find(
+        (identity) =>
+          identity.providerName && identity.providerName === provider
+      )
+    ) {
       return callback('No such link target', event);
     }
     // eslint-disable-next-line no-param-reassign
