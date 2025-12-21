5e6d7b803a043f8c9d02d86b36d89de5c9e3022e
Kenji Saito <ken-yo@mbr.nifty.com>
2025-12-21 11:58:54 +0900

    Bumps node modules

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,29 @@
-import nextPlugin from '@next/eslint-plugin-next';
-import reactPlugin from 'eslint-plugin-react';
-import hooksPlugin from 'eslint-plugin-react-hooks';
-import typescriptParser from '@typescript-eslint/parser';
+// @ts-check
 
-export default [
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
+// @ts-expect-error ignore plugin type
+import pluginPromise from 'eslint-plugin-promise';
+// import reactRefresh from "eslint-plugin-react-refresh";
+import nextPlugin from '@next/eslint-plugin-next';
+import globals from 'globals';
+import { configs, parser } from 'typescript-eslint';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, './.gitignore');
+
+export default defineConfig(
+  includeIgnoreFile(gitignorePath),
   {
-    languageOptions: {
-      parser: typescriptParser,
-    },
     ignores: [
       '**/*.d.ts',
       '*.js',
@@ -15,27 +31,60 @@ export default [
       'src/next-env.d.ts',
       'src/stories',
       'node_modules/**/*',
+      './.next/*',
     ],
+  },
+  eslint.configs.recommended,
+  ...configs.strict,
+  ...configs.stylistic,
+  pluginPromise.configs['flat/recommended'],
+  reactHooks.configs.flat.recommended,
+  // reactRefresh.configs.recommended,
+  {
     files: ['**/*.ts', '**/*.tsx'],
+    ...react.configs.flat.recommended,
+    ...react.configs.flat['jsx-runtime'],
+    languageOptions: {
+      ...react.configs.flat.recommended.languageOptions,
+      parser,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      formComponents: ['Form'],
+      linkComponents: [
+        { name: 'Link', linkAttribute: 'to' },
+        { name: 'NavLink', linkAttribute: 'to' },
+      ],
+      'import/internal-regex': '^~/',
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.tsx'],
+        },
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
     plugins: {
-      react: reactPlugin,
-      'react-hooks': hooksPlugin,
       '@next/next': nextPlugin,
+      '@stylistic': stylistic,
     },
     rules: {
-      ...reactPlugin.configs['jsx-runtime'].rules,
-      ...hooksPlugin.configs.recommended.rules,
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      '@next/next/no-duplicate-head': 'off',
-      '@next/next/no-img-element': 'error',
-      '@next/next/no-page-custom-font': 'off',
-      'no-html-link-for-pages': 'off',
-      'react-hooks/exhaustive-deps': 'off',
-      'react-hooks/rules-of-hooks': 'off',
+      '@stylistic/semi': 'error',
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/quotes': ['error', 'single'],
     },
   },
-  {
-    ignores: ['./.next/*'],
-  },
-];
+);
